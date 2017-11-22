bbd207f12dc5c729496d681d8908d427153cf084
D'Angelo Daniele <dangelodaniele25@gmail.com>
2017-11-22 09:36:20 +0100

    Commento iniziale e sul for

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+/*
+Programma fatto da Daniele D'Angelo
+*/
 #include<iostream>
 
 using namespace std;
@@ -5,7 +8,7 @@ using namespace std;
 void main()
 {
 	int contatore = 1;
-	for(int i = 0; i < 10; i++)
+	for(int i = 0; i < 10; i++)  //ciclo for per contare fino a 10
 	{
 		cout << contatore;
 		contatore++;
